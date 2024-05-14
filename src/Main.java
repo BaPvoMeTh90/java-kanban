db47e905bf27a3683e449872cca5da3fd1068b37
Oleg Pliskin Trifonov <Bapvometh90@yandex.ru>
2024-05-14 16:57:43 +0300

    История теперь содержит 1 экземпляр просмотренных задач. удаление просиходит за О(1).

--- src/Main.java
+++ src/Main.java
@@ -25,40 +25,6 @@ public class Main {
 
         System.out.println("история: " + taskManager.getHistory());
 
-        /*int epicId1 = taskManager.createEpic(new Epic("3", "3"));
-        Epic epic1 = taskManager.getEpic(epicId1);
-        System.out.println("пустой эпик:" + epic1);
-        System.out.println("история: " + taskManager.getHistory());
-        int epicId2 = taskManager.createEpic(new Epic("4", "4"));
-        Epic epic2 = taskManager.getEpic(epicId2);
-        int subTaskId1 = taskManager.createSubTask(new SubTask("5", "5", Status.NEW, epic1.getTaskId()));
-        SubTask subTask1 = taskManager.getSubTask(subTaskId1);
-        int subTaskId2 = taskManager.createSubTask(new SubTask("6", "6", Status.DONE, epic2.getTaskId()));
-        SubTask subTask2 = taskManager.getSubTask(subTaskId2);
-        int subTaskId3 = taskManager.createSubTask(new SubTask("7", "7", Status.NEW, epic2.getTaskId()));
-        System.out.println("история: " + taskManager.getHistory());
-        SubTask subTask3 = taskManager.getSubTask(subTaskId3);
-        System.out.println("список тасок:"+taskManager.getTasks());
-        System.out.println("список субтасок:"+taskManager.getSubTasks());
-        System.out.println("список эпиков:"+taskManager.getEpics());
-        task1.setTaskStatus(Status.DONE);
-        System.out.println("таска 1 поменяла статус"+task1);
-        subTask3.setTaskStatus(Status.DONE);
-        taskManager.updateSubTask(subTask3);
-        System.out.println("суб таска поменяла статус"+subTask3);
-        System.out.println("эпик поменял статус"+epic2);
-        taskManager.deleteTask(task1.getTaskId());
-        taskManager.deleteEpic(epic2.getTaskId());
-        System.out.println("удалил таску осталась таска 2"+taskManager.getTasks());
-        System.out.println("удалили эпик, поэтому удалены суб таски 6 и 7"+taskManager.getSubTasks());
-        System.out.println("удалил эпик остался 1"+taskManager.getEpics());
-        System.out.println("история: " + taskManager.getHistory());
-        Task task =new Task("название","ОПисание", Status.NEW);
-        taskManager.createTask(task);
-        taskManager.getTask(task.getTaskId());
-        task.setDescription("изменили описание");
-        taskManager.getTask(task.getTaskId());
-        System.out.println(taskManager.getHistory());*/
 
     }
 
--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -10,19 +10,19 @@ public class InMemoryHistoryManager implements HistoryManager {
     private static class Node {
         Task task;
         Node next;
-        Node perv;
+        Node prev;
 
-        Node(Node perv, Task task, Node next) {
-            this.perv = perv;
+        Node(Node prev, Task task, Node next) {
+            this.prev = prev;
             this.task = task;
             this.next = next;
         }
 
     }
 
-    HashMap<Integer, Node> history = new HashMap<>();
-    Node first;
-    Node last;
+    private final HashMap<Integer, Node> history = new HashMap<>();
+    private Node first;
+    private Node last;
 
 
     @Override
@@ -53,7 +53,7 @@ public class InMemoryHistoryManager implements HistoryManager {
         history.put(task.getTaskId(), newNode);
     }
 
-
+    @Override
     public void remove(int id) {
         removeNode(history.get(id));
     }
@@ -61,19 +61,19 @@ public class InMemoryHistoryManager implements HistoryManager {
     private void removeNode(Node node) {
         if (node != null) {
             history.remove(node.task.getTaskId());
-            Node pervNode = node.perv;
+            Node pervNode = node.prev;
             Node nextNode = node.next;
             if (pervNode == null) {
                 first = nextNode;
             } else {
                 pervNode.next = nextNode;
-                node.perv = null;
+                node.prev = null;
             }
 
             if (nextNode == null) {
                 last = pervNode;
             } else {
-                nextNode.perv = pervNode;
+                nextNode.prev = pervNode;
                 node.next = null;
             }
         }
@@ -81,11 +81,13 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void historyAdd(Task task) {
-        Node node = history.get(task.getTaskId());
-        if (node != null) {
-            removeNode(node);
+        if (task != null) {
+            Node node = history.get(task.getTaskId());
+            if (node != null) {
+                removeNode(node);
+            }
+            linkLast(task);
         }
-        linkLast(task);
     }
 }
 
