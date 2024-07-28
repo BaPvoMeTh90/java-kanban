29801850f39f497ba06299ff35ee93161f905b4b
Oleg Pliskin Trifonov <Bapvometh90@yandex.ru>
2024-07-28 19:10:39 +0300

    ТЗ8 Реализовваны: ввременные теги в задачаз, их начало/конец и продолжительнолсть. Приоритизация задач по времени.

--- test/model/TaskTest.java
+++ test/model/TaskTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import service.Managers;
 import service.TaskManager;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Таск")
@@ -15,9 +18,9 @@ class TaskTest {
     @Test
     @DisplayName("Таск должен совпадать с копией")
     void shouldBeEqualsToCopy (){
-        Task task = new Task("название1", "описание1", Status.NEW);
+        Task task = new Task("название1", "описание1", Status.NEW, LocalDateTime.now(), Duration.ofMinutes(0));
         int taskId = taskManager.createTask(task);
-        Task taskToComparison = new Task(1, "название", "описание", Status.DONE);
+        Task taskToComparison = new Task(1, "название", "описание", Status.DONE, LocalDateTime.now(), Duration.ofMinutes(0));
         assertEquals(task, taskToComparison, "Сравнение по: id");
     }
 }
