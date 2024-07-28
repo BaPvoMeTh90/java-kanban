dfeba75bd55ab4bb5e98d2f2a0b74d5c73d70294
Oleg Pliskin Trifonov <Bapvometh90@yandex.ru>
2024-07-28 19:05:15 +0300

    ТЗ8 Реализовваны: ввременные теги в задачаз, их начало/конец и продолжительнолсть. Приоритизация задач по времени.

--- test/service/FileBackedTasksManagerTest.java
+++ test/service/FileBackedTasksManagerTest.java
@@ -4,49 +4,51 @@ import model.Epic;
 import model.Status;
 import model.SubTask;
 import model.Task;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class FileBackedTasksManagerTest {
+@DisplayName("Менеджер сохранения задач в файл тест")
+class FileBackedTasksManagerTest extends TaskManagerTest<InMemoryTaskManager> {
 
-    TaskManager manager = Managers.getDefault();
-    TaskManager loadedManager;
-    @AfterEach
+    File temp;
 
+    @BeforeEach
+    void BeforeEach() throws IOException {
+        temp = File.createTempFile("testToLoad.", "csv");
+        taskManager = new FileBackedTasksManager(Managers.getDefaultHistory(), temp);
+    }
+
+
+    @Test
+    @DisplayName("не загружается когда нет файла для загрузки")
+    void whenLoadTasksAndFileNotExists() {
+        assertThrows(ManagerLoadException.class, () -> {
+            FileBackedTasksManager.loadFromFile(new File("Path/notExistPath"));
+        }, "Не удалось загрузить данные");
+    }
 
     @Test
     @DisplayName("сохраненные и загруженные таски одинаковые")
     public void saveAndLoadedTaskAreEqual() {
-        int taskId1 = manager.createTask(new Task(" ", " ", Status.NEW));
-        Task task1 = manager.getTask(taskId1);
-        int taskId2 = manager.createEpic(new Epic(" ", " "));
-        Epic task2 = manager.getEpic(taskId2);
-        int taskId3 = manager.createSubTask(new SubTask("", "", Status.NEW, 2));
-        SubTask task3 = manager.getSubTask(taskId3);
-        loadedManager = FileBackedTasksManager.loadFromFile(new File("resources/toLoad.csv"));
-        assertEquals(manager.getTask(taskId1).toString(), loadedManager.getTask(taskId1).toString(), "Таски не одинаковы");
-        assertEquals(manager.getEpic(taskId2).toString(), loadedManager.getEpic(taskId2).toString(), "Таски не одинаковы");
-        assertEquals(manager.getSubTask(taskId3).toString(), loadedManager.getSubTask(taskId3).toString(), "Таски не одинаковы");
+        int taskId1 = taskManager.createTask(new Task(" ", " ", Status.NEW, LocalDateTime.now(), Duration.ofMinutes(0)));
+        Task task1 = taskManager.getTask(taskId1);
+        int taskId2 = taskManager.createEpic(new Epic(" ", " "));
+        Epic task2 = taskManager.getEpic(taskId2);
+        int taskId3 = taskManager.createSubTask(new SubTask("", "", Status.NEW, LocalDateTime.now().plusMinutes(1), Duration.ofMinutes(0), 2));
+        SubTask task3 = taskManager.getSubTask(taskId3);
+        TaskManager loadedManager = FileBackedTasksManager.loadFromFile(temp);
+        assertEquals(taskManager.getTask(taskId1).toString(), loadedManager.getTask(taskId1).toString(), "Таски не одинаковы");
+        assertEquals(taskManager.getEpic(taskId2).toString(), loadedManager.getEpic(taskId2).toString(), "Таски не одинаковы");
+        assertEquals(taskManager.getSubTask(taskId3).toString(), loadedManager.getSubTask(taskId3).toString(), "Таски не одинаковы");
     }
 
-    @Test
-    @DisplayName("сохранение и загрузку пустого файла")
-    public void saveAbdLoadEmptyFile() throws IOException {
-        loadedManager = FileBackedTasksManager.loadFromFile(File.createTempFile("resources/", "TestToLoad.csv"));
-        List<Task> list = loadedManager.getTasks();
-        int lengthShouldBe = 0;
-        assertEquals(lengthShouldBe, list.size(), "загружн не пустой файл");
-        int taskID = loadedManager.createTask(new Task("", "", Status.NEW));
-
-        List<Task> list1 = loadedManager.getTasks();
-        int lengthShouldBe1 = 1;
-        assertEquals(lengthShouldBe1, list1.size(), "файл не записан");
-    }
 }
